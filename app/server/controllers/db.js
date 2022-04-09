73ec68dcf4a9d79e0dfb87be34e8140d82ade2ab
renatus-cartesius <cartesius.absolute@gmail.com>
2022-04-09 21:43:14 +0300

    Adding some more files

---
+++ app/server/controllers/db.js
@@ -0,0 +1,13 @@
+var mariadb = require("mariadb")
+var pool = 
+	mariadb.createPool({
+		host: "127.0.0.1",
+		port: 3306,
+		user: "mansys",
+		password: "root",
+		database: "man_sys_db"
+	});
+
+module.exports = Object.freeze({
+	pool: pool
+});
