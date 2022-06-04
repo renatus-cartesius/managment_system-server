919c2d2ce79096d437218f5712007f6202c8c488
renatus-cartesius <cartesius.absolute@gmail.com>
2022-06-04 20:41:28 +0300

    Adding some employees functional

--- app/server/routes/employee.routes.js
+++ app/server/routes/employee.routes.js
@@ -24,4 +24,29 @@ router.get("/employee", async (req, res)=>{
 	}
 });
 
+router.post("/create_employee", async (req, res)=>{
+	let name = req.body.name;
+	let surname = req.body.surname;
+	let phone = req.body.phone;
+	let email = req.body.email;
+	let did = req.body.did;
+	let position = req.body.position;
+	try{
+		const result = await db.pool.query(`insert into Employees (Name, Surname, Phone, Email, DID, Position) values (\"${name}\", \"${surname}\", \"${phone}\", \"${email}\", \"${did}\", \"${position}\")`);
+		res.send(result.status);
+	}catch(err){
+		throw err;
+	}
+});
+
+router.post("/delete_employee", async (req, res)=>{
+	try{
+		console.log(`[-] Deleting Employee with ID: ${req.body.eid}`);
+		const result = await db.pool.query(`delete from Employees where ID=\"${req.body.eid}\"`);
+		res.send(result.status);
+	}catch(err){
+		throw err;
+	}
+});
+
 module.exports = router;
